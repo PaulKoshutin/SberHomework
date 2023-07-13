7a0c13008fec834917d8631e099b61e33c2a08f5
PaulKoshutin <koshutin.pavel@yandex.ru>
2023-07-13 17:42:33 +0300

    MinorAddition

---
+++ Reflection/src/test/java/ReflectionTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReflectionTest {
+    @Test
+    public void ReflectionTester() throws NoSuchFieldException, NoSuchMethodException {
+        InheritedChildClassOverride child1 = new InheritedChildClassOverride();
+        InheritedChildClassStd child2 = new InheritedChildClassStd();
+
+        child1.methodB("R",42,1.1);
+        child2.methodA("R",42,1.1);
+
+        System.out.println(child1.dataContainer.getDataName()+" "+child1.dataContainer.getDataNumber()+" "+child1.dataContainer.getDataValue());
+        System.out.println(child2.dataContainer.getDataName()+" "+child2.dataContainer.getDataNumber()+" "+child2.dataContainer.getDataValue());
+        System.out.println();
+
+        assertEquals(child1.dataContainer.getDataName(),"R");
+        assertEquals(child2.dataContainer.getDataName(),"");
+
+        NewRepeatableAnnotation[] annotations = child1.getClass().getMethod("methodB", String.class, int.class, double.class).getAnnotation(NewRepeatableAnnotations.class).value();
+        Arrays.sort(annotations, Comparator.comparing(NewRepeatableAnnotation::priority));
+        for (NewRepeatableAnnotation method:annotations) {
+            child1.methodB(method.dataName(), method.dataNumber(), method.dataValue());
+            System.out.println(child1.dataContainer.getDataName()+" "+child1.dataContainer.getDataNumber()+" "+child1.dataContainer.getDataValue());
+        }
+    }
+}
